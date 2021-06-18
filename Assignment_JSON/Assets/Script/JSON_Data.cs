b98aa043dea8c7e546dbfa83a3b582fae3cbd74f
senarup7 <senarup7@gmail.com>
2021-06-18 11:19:06 +0530

    Add files via upload

---
+++ Assignment_JSON/Assets/Script/JSON_Data.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class JSON_Data : MonoBehaviour
+{
+
+    public Text MessageOfTheDay;
+    string json;
+    public List<Root> _Root;
+
+    public GameObject cell;
+    public GameObject content;
+
+    int DataCount;
+    Root response;
+    // Use this for initialization
+    void Start()
+    {
+
+        
+        StartCoroutine(GetRequest("https://api.jsonbin.io/b/5f56071c4d8ce411138a7d14"));
+    }
+
+    int n = 0;
+    IEnumerator GetRequest(string uri)
+    {
+        MessageOfTheDay.text = "Loading... Please Wait";
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                json = webRequest.downloadHandler.text;
+                Debug.Log(pages[page] + ":\nReceived: " + json);
+                
+                response = JsonConvert.DeserializeObject<Root>(json);
+
+                DataCount = response.items.Count;
+
+            }
+        }
+        if (response.items.Count != 0)
+        {
+            Debug.Log("Data Pulled Success");
+            Invoke("SetDataToUI", 1f);
+        }
+        else
+        {
+            MessageOfTheDay.text = "Loading Faild or Network Error";
+        }
+       
+    }
+  
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void SetDataToUI()
+    {
+        SetData();
+       // StartCoroutine(setImage(imageLink, DataCount));
+    }
+    void SetData()
+    {
+
+        MessageOfTheDay.text = response.messageOfTheDay;
+       for (int i = 0; i < DataCount; i++)
+        {
+
+            string path = response.imagePath.ToString() + response.items[i].image.ToString();
+            string title = response.items[i].title.ToString();
+            GameObject row = Instantiate(cell);
+            row.transform.SetParent(content.transform,false);
+            row.GetComponent<TableData>().SetData(path,title,response.items[i].address, response.items[i].available); 
+        }
+    }
+
+   
+}
+
+
+[System.Serializable]
+public class Item
+{
+    public string title { get; set; }
+    public string image { get; set; }
+    public bool available { get; set; }
+    public string address { get; set; }
+}
+
+[System.Serializable]
+public class Root
+{
+    public string messageOfTheDay { get; set; }
+    public string imagePath { get; set; }
+    public List<Item> items { get; set; }
+}
+
+
---
+++ Assignment_JSON/Assets/Script/TableData.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+public class TableData : MonoBehaviour
+{
+    [SerializeField]
+    Sprite LoackImage;
+
+    public RowData rowData;
+    Root response;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SetData(string url,string title, string address, bool available)
+    {
+        StartCoroutine(setImage(url,title,address,available));
+    }
+    public IEnumerator setImage(string url, string title,string address, bool available)
+    {
+
+            Texture2D tex;
+            tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+            //url = response.imagePath + response.items[i].image;
+            using (WWW www = new WWW(url))
+            {
+                yield return www;
+                www.LoadImageIntoTexture(tex);
+
+
+                rowData.SL_NO.texture = tex;
+                rowData.TITLE.text = title;
+                
+                if (!available)
+                {
+                    rowData.LOCK.overrideSprite = LoackImage;
+                    rowData.ADDRESS.text = null;
+                    rowData.button.interactable = false;
+            }
+                else
+                {
+                    rowData.LOCK.transform.gameObject.SetActive(false);
+                    rowData.ADDRESS.text = address;
+                    rowData.button.interactable =  true;
+                    
+                }
+                
+            }
+        
+    }
+
+    public void OnClick()
+    {
+        Application.OpenURL(rowData.ADDRESS.text);
+    }
+}
+[System.Serializable]
+public class RowData
+{
+    public RawImage SL_NO;
+    public Button button;
+    public Text TITLE;
+    public Image LOCK;
+    public Text ADDRESS;
+    public string AVAILABLE;
+}
